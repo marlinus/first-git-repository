7a9f0e6c27ed229517814cabfff9765bac800999
Robert <tre8borrobert@yandex.ru>
2022-07-27 22:22:58 +0300

    My new update

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-console.log('hello git');
-console.log('hello git 2');
-console.log('hello git 3!!!!!!!');
+const express = require('express');
+
+const PORT = 3003;
+const HOST = 'localhost';
+
+const app = express();
+
+app.get('/', (req, res) => {
+	console.log(req.url);
+	res.send('<h1>Test page</h1>');
+});
+
+app.listen(PORT, HOST, () => {
+	console.log('Server working...');
+});
